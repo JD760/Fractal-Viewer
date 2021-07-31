5b51a08ac6d3f4707efe05acbef5267a6cf41f1b
JacobD <jacobdicken2005@gmail.com>
2021-07-31 01:25:24 +0100

    Daily commit from laptop, modified binomial series algorithm - now generates powers correctly but fails when computing n choose k

--- dev/binomial.c
+++ dev/binomial.c
@@ -18,22 +18,55 @@ typedef struct Term{
 }Term;
 /* globals */
 /* function prototypes */
+void binomialCoefficients(int a, int b, int n, Term *result);
+int factorial(int n);
 
 void main()
 {
-    return;
+    int n = 4;
+    Term *result = malloc(sizeof(Term) * (n + 1));
+    binomialCoefficients(3, 6, n, result);
+    //printf("Factorial: %d\n", factorial(4));
+    /*
+    for(int i = 0; i < n; i++)
+    {
+        printf("(%d)(x^%d)(y^%d)\n", result[i].choose, result[i].a, result[i].b);
+    }
+    return; */
 }
 /*
  * find the result of (a + b)^n
+ * each item in result array represents 
+ * (n choose k)(x^a)(y^b) eg 1x^2b^3
 */
-void binomialCoefficients(int a, int b, int n)
+void binomialCoefficients(int a, int b, int n, Term *result)
 {
-    // there are n+1 terms produced from (a + b)^n expansion
-    Term *result = malloc(sizeof(Term) * (n + 1));
+    int chooseDenominator;
+
     for(int k = 0; k <= n; k++)
     {
-        result[0].a = n - k;
-        result[0].b = k;
-        
+        //printf("%d\n", k);
+        result[k].a = n - k;
+        result[k].b = k;
+        result[k].choose;
+        // save on computations by using known values for the denominator
+        if (k == 0 || k == n) result[k].choose = 1;
+        else if (k == 1 || k == n - 1) result[k].choose = n;
+        else {
+            chooseDenominator = factorial(k) * factorial(n - k);
+            result[k].choose = factorial(n) / chooseDenominator;
+        }
+        printf("(%d)(x^%d)(y^%d)\n", result[k].choose, result[k].a, result[k].b);
     }
+    
+}
+
+int factorial(int n)
+{
+    int output = 1;
+    for(int i = n; i > 1; i--)
+    {
+        output *= i;
+    }
+    return output;
 }
